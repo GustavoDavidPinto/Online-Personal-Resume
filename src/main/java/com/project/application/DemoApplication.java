3cdf1613fc226b0bac0b919ea902617251e8ddd3
Gustavo David <gustavodavidpinto@gmail.com>
2026-09-23 16:29:03 -0300

    Organizando paginas e pacotes

--- src/main/java/com/project/application/DemoApplication.java
+++ src/main/java/com/project/application/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.personal_resume.demo;
+package com.project.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
